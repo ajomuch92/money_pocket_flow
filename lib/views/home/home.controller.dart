8dc229cc4ca292ec1277eb019b54f57708a47ac2
ajomuch92 <aaronmontes_92@yahoo.es>
2024-12-15 21:23:26 -0600

    feat: 🎸 Adding more logic and widgets to home view and controll

--- lib/views/home/home.controller.dart
+++ lib/views/home/home.controller.dart
@@ -1,5 +1,54 @@
 import 'package:flutter_solidart/flutter_solidart.dart';
+import 'package:money_pocket_flow/dto/settings.repository.dart';
+import 'package:money_pocket_flow/dto/transaction.repository.dart';
+import 'package:money_pocket_flow/models/index.dart';
+import 'package:money_pocket_flow/models/utilities_class.dart';
 
 class HomeController {
   Signal<int> totalExpends = Signal(0);
+  Signal<Settings> settings = Signal(Settings());
+  Signal<List<bool>> listSelectedOption = Signal([true, false, false]);
+  late Resource<Settings> resourceSettings;
+  late Resource<InOutResult> resourceInOut;
+  late Resource<List<Category>> resourceCategories;
+  final settingRepo = SettingsRepository();
+  final transactionsRepo = TransactionRepository();
+
+  HomeController() {
+    resourceSettings = Resource(fetcher: loadSettings);
+    resourceInOut = Resource(fetcher: getTotals, source: listSelectedOption);
+    resourceCategories =
+        Resource(fetcher: getTotalsByCategory, source: listSelectedOption);
+    getTotalsByCategory();
+  }
+
+  FilterDateType get filterDate {
+    final listValues = listSelectedOption.value;
+    if (listValues[0]) {
+      return FilterDateType.currentMonth;
+    } else if (listValues[1]) {
+      return FilterDateType.lastWeek;
+    }
+    return FilterDateType.today;
+  }
+
+  Future<Settings> loadSettings() async {
+    return await settingRepo.getSettings();
+  }
+
+  Future<InOutResult> getTotals() async {
+    return await transactionsRepo.getTotalsFiltered(type: filterDate);
+  }
+
+  Future<List<Category>> getTotalsByCategory() async {
+    return await transactionsRepo.getTotalsByCategory(type: filterDate);
+  }
+
+  void setSelectedOption(int index) {
+    listSelectedOption.value = [
+      0 == index,
+      1 == index,
+      2 == index,
+    ];
+  }
 }
--- lib/views/home/home.view.dart
+++ lib/views/home/home.view.dart
@@ -1,6 +1,10 @@
+// ignore_for_file: unnecessary_brace_in_string_interps
+
+import 'package:fluentui_emoji_icon/fluentui_emoji_icon.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_solidart/flutter_solidart.dart';
+import 'package:getwidget/getwidget.dart';
+import 'package:money_pocket_flow/shared/ErrorEmpty.dart';
 import 'package:money_pocket_flow/views/home/home.controller.dart';
 
 class Home extends StatefulWidget {
@@ -15,49 +19,175 @@ class _HomeState extends State<Home> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.symmetric(horizontal: 20.0),
-      width: MediaQuery.sizeOf(context).width,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          const Text(
-            'Gastos Totales',
-            style: TextStyle(fontWeight: FontWeight.w600),
-          ),
-          SignalBuilder<int>(
-            signal: controller.totalExpends,
-            builder: (context, value, child) {
-              return Text(
-                '$value',
-                style: const TextStyle(
-                  fontWeight: FontWeight.w500,
-                  fontSize: 32,
-                ),
-              );
-            },
-          ),
-          const SizedBox(
-            height: 200.0,
-            child: Placeholder(),
-          ),
-          const ListTile(
-            title: Text('Gastos'),
-            trailing: Text('\$0'),
-          ),
-          const ListTile(
-            title: Text('Ingresos'),
-            trailing: Text('\$0'),
-          ),
-          ListTile(
-            title: const Text(
-              'Detalles',
-              style: TextStyle(fontWeight: FontWeight.bold),
-            ),
-            trailing: Text('\$0'),
-          ),
-        ],
-      ),
-    );
+    return ResourceBuilder(
+        resource: controller.resourceSettings,
+        builder: (context, settingsState) {
+          return settingsState.on(
+              ready: (settings) {
+                String currencySymbol = settings.currencySymbol ?? '\$';
+                return Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                  width: MediaQuery.sizeOf(context).width,
+                  child: ResourceBuilder(
+                      resource: controller.resourceInOut,
+                      builder: (context, inOutState) {
+                        return inOutState.on(
+                            ready: (totals) => SingleChildScrollView(
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.spaceBetween,
+                                        children: [
+                                          Column(
+                                            children: [
+                                              const Text(
+                                                'Gastos Totales',
+                                                style: TextStyle(
+                                                    fontWeight:
+                                                        FontWeight.w600),
+                                              ),
+                                              Text(
+                                                '${currencySymbol} ${totals.outTotal}',
+                                                style: const TextStyle(
+                                                  fontWeight: FontWeight.w500,
+                                                  fontSize: 32,
+                                                ),
+                                              ),
+                                            ],
+                                          ),
+                                          SignalBuilder(
+                                              signal:
+                                                  controller.listSelectedOption,
+                                              builder: (context,
+                                                  selectedOptions, _) {
+                                                return ToggleButtons(
+                                                  isSelected: selectedOptions,
+                                                  borderRadius:
+                                                      const BorderRadius.all(
+                                                          Radius.circular(8)),
+                                                  onPressed: controller
+                                                      .setSelectedOption,
+                                                  constraints:
+                                                      const BoxConstraints(
+                                                    minHeight: 25.0,
+                                                    minWidth: 50.0,
+                                                  ),
+                                                  children: const <Widget>[
+                                                    Text('Mes'),
+                                                    Text('Semana'),
+                                                    Text('Hoy'),
+                                                  ],
+                                                );
+                                              }),
+                                        ],
+                                      ),
+                                      const SizedBox(
+                                        height: 200.0,
+                                        child: Placeholder(),
+                                      ),
+                                      ListTile(
+                                        title: const Text('Gastos'),
+                                        trailing: Text(
+                                            '${currencySymbol} ${totals.outTotal}'),
+                                      ),
+                                      ListTile(
+                                        title: const Text('Ingresos'),
+                                        trailing: Text(
+                                            '${currencySymbol} ${totals.inTotal}'),
+                                      ),
+                                      const Divider(),
+                                      Padding(
+                                        padding:
+                                            const EdgeInsets.only(left: 15),
+                                        child: Row(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.spaceBetween,
+                                          children: [
+                                            const Text(
+                                              'Detalles',
+                                              style: TextStyle(
+                                                  fontWeight: FontWeight.bold),
+                                            ),
+                                            IconButton(
+                                              onPressed: () {},
+                                              icon: const Icon(
+                                                  Icons.chevron_right),
+                                            )
+                                          ],
+                                        ),
+                                      ),
+                                      ResourceBuilder(
+                                        resource: controller.resourceCategories,
+                                        builder: (context, categoriesState) {
+                                          return categoriesState.on(
+                                              ready: (categories) => categories
+                                                      .isEmpty
+                                                  ? const ErrorEmpty(
+                                                      message:
+                                                          'No hay transacciones guardadas',
+                                                      fullHeight: false,
+                                                      child: FluentUiEmojiIcon(
+                                                        fl: Fluents
+                                                            .flFaceWithDiagonalMouth,
+                                                        w: 50,
+                                                        h: 50,
+                                                      ))
+                                                  : ListView.builder(
+                                                      shrinkWrap: true,
+                                                      itemBuilder:
+                                                          (context, index) {
+                                                        final category =
+                                                            categories[index];
+                                                        return ListTile(
+                                                          leading: Text(
+                                                            category.name!,
+                                                            style:
+                                                                const TextStyle(
+                                                              fontSize: 16,
+                                                            ),
+                                                          ),
+                                                          trailing: Text(
+                                                            "$currencySymbol ${category.amount!}",
+                                                            style:
+                                                                const TextStyle(
+                                                              fontSize: 16,
+                                                              fontWeight:
+                                                                  FontWeight
+                                                                      .bold,
+                                                            ),
+                                                          ),
+                                                        );
+                                                      },
+                                                      itemCount:
+                                                          categories.length,
+                                                    ),
+                                              error: (e, t) => const ErrorEmpty(
+                                                  message:
+                                                      'Hubo un error al cargar los detalles'),
+                                              loading: () => const Center(
+                                                    child: GFLoader(),
+                                                  ));
+                                        },
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                            error: (e, t) => const ErrorEmpty(
+                                message: 'Hubo un error al cargar los datos'),
+                            loading: () => const Center(
+                                  child: GFLoader(),
+                                ));
+                      }),
+                );
+              },
+              error: (e, t) => const ErrorEmpty(
+                  message: 'Hubo un error al cargar las configuraciones'),
+              loading: () => const Center(
+                    child: GFLoader(),
+                  ));
+        });
   }
 }
