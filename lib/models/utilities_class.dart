f120b15ba1a23d6c54de31e05bb9f0d477b52ecb
ajomuch92 <aaronmontes_92@yahoo.es>
2024-12-17 08:15:47 -0600

    feat: 🎸 Adding class and enum

--- lib/models/utilities_class.dart
+++ lib/models/utilities_class.dart
@@ -4,3 +4,29 @@ class InOutResult {
 
   InOutResult({this.inTotal = 0, this.outTotal = 0});
 }
+
+enum TransactionType {
+  inTrx('in'),
+  outTrx('out');
+
+  const TransactionType(this.str);
+  final String str;
+}
+
+class TransactionFilter {
+  int? categoryId;
+  DateTime? initialDate;
+  DateTime? endDate;
+  TransactionType? type;
+  int? page;
+  int? limit;
+
+  TransactionFilter({
+    this.categoryId,
+    this.initialDate,
+    this.endDate,
+    this.type,
+    this.page,
+    this.limit,
+  });
+}
